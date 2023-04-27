37aab6f1597416a32818f2e34c8c62cb23380147
rethred <gabrielwastha@gmail.com>
2023-04-27 12:05:53 -0300

    testing category entity to ensure its behavior

--- src/modules/category/domain/entities/category.entity.spec.ts
+++ src/modules/category/domain/entities/category.entity.spec.ts
@@ -3,22 +3,75 @@ import { CategoryEntity } from "./category.entity"
 
 describe("test CategoryEntity", () => {
 
-    it("Should create a CategoryEntity", () => {
-        const props = {
+    let sut: CategoryEntity
+    let props: CategoryEntity.Input
+
+    const makeSut = (props: CategoryEntity.Input) => {
+        const categoryEntityOrError = CategoryEntity.create(props)
+        if(categoryEntityOrError.isLeft()) throw categoryEntityOrError.value[0]
+        sut = categoryEntityOrError.value
+        return sut
+    }
+
+    beforeEach(() => {
+        props = {
             title: "any_title",
             description: "any_description",
-            parrentId: "any_parrent_id"
         }
+        sut = makeSut(props)
+    })
+
+    it("Should create a CategoryEntity", () => {
         const sut = CategoryEntity.create(props, "any_id")
-    
         if(sut.isLeft()) throw sut.value[0]
-    
+
         expect(sut.value.toJSON()).toEqual({
             id: "any_id",
             title: props.title,
             description: props.description,
-            parrentId: props.parrentId
+            parrentId: props.parrentId,
+            status: "DEACTIVE"
         })
     })
+
+    it("Should be a subCategory if parrentId is provided", () => {
+        const sut  = makeSut({
+            ...props,
+            parrentId: "any_parrent_id",
+        })
+        expect(sut.isSubCategory()).toBe(true)
+    })
+
+    it("Should not be a subCategory if parrentId is not provided", () => {
+        const sut  = makeSut({
+            ...props
+        })
+        expect(sut.isSubCategory()).toBe(false)
+    })
+
+    it("Should activate a category", () => {
+        expect(sut.isActivate()).toBe(false)
+        sut.activate()
+        expect(sut.isActivate()).toBe(true)
+    })
+
+    it("Should deactivate a category", () => {
+        sut.activate()
+        expect(sut.isActivate()).toBe(true)
+        sut.deactivate()
+        expect(sut.isActivate()).toBe(false)
+    })
+
+    it("Should check if the category is active", () => {
+        expect(sut.status).toBe("DEACTIVE")
+        expect(sut.isActivate()).toBe(false)
+        
+        sut.activate()
+        expect(sut.status).toBe("ACTIVE")
+        expect(sut.isActivate()).toBe(true)
+    })
+
+
+
 
 })
--- src/modules/category/domain/entities/category.entity.ts
+++ src/modules/category/domain/entities/category.entity.ts
@@ -9,8 +9,25 @@ export class CategoryEntity extends BaseEntity<CategoryEntity.Props> {
 
     static create(input: CategoryEntity.Input, id?: string): Either<Error[], CategoryEntity>{
 
-        const categoryEntity = new CategoryEntity(input, id)
+        const categoryEntity = new CategoryEntity({
+            ...input,
+            status: "DEACTIVE"
+        }, id)
         return right(categoryEntity) 
+    }
+
+    activate(): void{
+        this.props.status = "ACTIVE"
+    }
+    deactivate(): void{
+        this.props.status = "DEACTIVE"
+    }
+    
+    isActivate(): boolean {
+        return this.props.status === "ACTIVE"
+    }
+    isSubCategory(): boolean {
+        return !!this.parrentId
     }
     
     toJSON(): CategoryEntity.PropsJSON {
@@ -18,7 +35,8 @@ export class CategoryEntity extends BaseEntity<CategoryEntity.Props> {
             id: this.id,
             title: this.title,
             description: this.description,
-            parrentId: this.parrentId
+            parrentId: this.parrentId,
+            status: this.status
         }
     }
 
@@ -31,10 +49,15 @@ export class CategoryEntity extends BaseEntity<CategoryEntity.Props> {
     get parrentId(): string | undefined {
         return this.props.parrentId
     }
+    get status(): CategoryEntity.status {
+        return this.props.status
+    }
 }
 
 export namespace CategoryEntity {
     
+    export type status = "DEACTIVE" | "ACTIVE" 
+
     export type Input = {
         title: string
         description: string
@@ -45,6 +68,7 @@ export namespace CategoryEntity {
         title: string
         description: string
         parrentId?: string
+        status: status
     }
 
     export type PropsJSON = Props & { id: string }
