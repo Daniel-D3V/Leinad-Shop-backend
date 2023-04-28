77faf93a38558e6469924f21e599e9eb1239dbbc
rethred <gabrielwastha@gmail.com>
2023-04-28 19:22:57 -0300

    begin integration tests on prisma-category

--- src/modules/category/infra/repositories/prisma/prisma-category.repository.test.ts
+++ src/modules/category/infra/repositories/prisma/prisma-category.repository.test.ts
@@ -1,17 +1,45 @@
 import { CategoryEntity } from "@/modules/category/domain/entities"
 import { PrismaCategoryRepository } from "./prisma-category.repository"
-
+import { mock } from "jest-mock-extended"
+import { randomUUID } from "crypto"
+import { prismaClient } from "@/modules/@shared/infra/repository/prisma/client"
 
 describe("Test prismaCategoryRepository integration with mysql", () => {
 
     let sut: PrismaCategoryRepository
     let categoryEntity: CategoryEntity
+    let props: CategoryEntity.PropsJSON
 
-    beforeAll(() => {
+    beforeEach(async () => {
+        props = {
+            id: "any_id",
+            description: "any description",
+            status: "ACTIVE",
+            title: "any title",
+        }
+        categoryEntity = mock<CategoryEntity>({
+            toJSON: () => props
+        })
         sut = new PrismaCategoryRepository()
+        await prismaClient.category.deleteMany()
     })
     
-    it("Should ", async () => {
-       await sut.create(categoryEntity)
+    it("Should persist the category on the database", async () => {
+        await sut.create(categoryEntity)
+        const category = await prismaClient.category.findUnique({
+            where: {
+                id: "any_id"
+            }
+        })
+        expect({
+            ...category,
+            parentId: undefined
+        }).toEqual(props)
+    })
+
+    it("Should find by categoryId", async () => {
+        await sut.create(categoryEntity)
+        const categoryEntityFound = await sut.findById("any_id")
+        expect(categoryEntityFound).toBeInstanceOf(CategoryEntity)
     })
 })
--- src/modules/category/infra/repositories/prisma/prisma-category.repository.ts
+++ src/modules/category/infra/repositories/prisma/prisma-category.repository.ts
@@ -1,17 +1,36 @@
 import { prismaClient } from "@/modules/@shared/infra/repository/prisma/client";
 import { CategoryEntity } from "@/modules/category/domain/entities";
 import { CategoryRepositoryInterface } from "@/modules/category/domain/repositories";
+import { Category } from "@prisma/client";
+
+const setStatus = (categoryEntity: CategoryEntity, prismaCategory: Category) => {
+
+}
 
 export class PrismaCategoryRepository implements CategoryRepositoryInterface {
     async findByTitle(name: string): Promise<CategoryEntity | null> {
         return null
     }
-    findById(id: string): Promise<CategoryEntity | null> {
-        throw new Error("Method not implemented.");
+    async findById(id: string): Promise<CategoryEntity | null> {
+        const prismaCategory = await prismaClient.category.findFirst({
+            where: { id }
+        })
+        if(!prismaCategory) return null
+        const categoryEntity = CategoryEntity.create({
+            ...prismaCategory
+        }, prismaCategory.id)
+        if(categoryEntity.isLeft()) throw categoryEntity.value[0]
+
+        setStatus(categoryEntity.value, prismaCategory)
+
+        return categoryEntity.value
     }
     async create(category: CategoryEntity): Promise<void> {
-        const output = await prismaClient.category.findFirst({ where: { id: "fs" } })
-        console.log(output)
+        await prismaClient.category.create({ 
+            data: {
+                ...category.toJSON(),
+            }
+        })
     }
     delete(id: string): Promise<void> {
         throw new Error("Method not implemented.");
