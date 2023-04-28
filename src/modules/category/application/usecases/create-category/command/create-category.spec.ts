f4e40aac53ac0efc6dfbdbd88731cb2717a6a08c
rethred <gabrielwastha@gmail.com>
2023-04-28 10:04:41 -0300

    finish unity test to create category and to persist category

--- src/modules/category/application/usecases/create-category/command/create-category.spec.ts
+++ src/modules/category/application/usecases/create-category/command/create-category.spec.ts
@@ -4,10 +4,11 @@ import { CreateCategoryUsecase } from "./create-category.usecase";
 import { mock } from 'jest-mock-extended';
 import { CategoryRepositoryInterface } from "@/modules/category/domain/repositories";
 import { CommandEmitterInterface } from "@/modules/@shared/events";
-import { right } from "@/modules/@shared/logic";
+import { CreateCategoryCommand } from "./create-category.command";
 
 
 jest.mock("@/modules/category/domain/entities")
+jest.mock("./create-category.command")
 
 describe("Test CreateCategoryUsecase", () => {
 
@@ -22,7 +23,7 @@ describe("Test CreateCategoryUsecase", () => {
         categoryEntity.create.mockReturnValue({
             isLeft: () => false,
             value: {
-                toJSON: () => "entityValue",
+                toJSON: () => ({ anyEntityToJSONValue: "any" }),
             }
         } as any)
 
@@ -40,4 +41,39 @@ describe("Test CreateCategoryUsecase", () => {
         expect(output.isRight()).toBe(true)
     })
 
+    it("Should return left if the categoryEntity is invalid", async () => {
+        categoryEntity.create.mockReturnValueOnce({
+            isLeft: () => true,
+            value: [ new Error("EntityError") ]
+        } as any)
+        const output = await sut.execute(props)
+        if(output.isRight()) throw new Error("usecase should not return right")
+
+        expect(output.isLeft()).toBe(true)
+        expect(output.value[0]).toEqual(new Error("EntityError"))
+    })
+
+    it("Should return CategoryTitleInUseError if categoryRepository finds a category using a title", async () => {
+        const categoryRepositorySpy = jest.spyOn(categoryRepository, "findByTitle")
+        .mockReturnValueOnce(true as any)
+
+        const output = await sut.execute(props)
+        if(output.isRight()) throw new Error("usecase should not return right")
+
+        expect(output.value[0].name).toBe("CategoryTitleInUseError")
+        expect(categoryRepositorySpy).toHaveBeenCalledTimes(1)
+    })
+
+    it("Should call commandEmitter once", async () => {
+        const commandEmitterSpy = jest.spyOn(commandEmitter, "emit")
+        await sut.execute(props)
+        expect(commandEmitterSpy).toHaveBeenCalledTimes(1)
+    })
+
+    it("Should create CreateCategoryCommand with correct values", async () => {
+        
+        await sut.execute(props)
+        expect(CreateCategoryCommand).toHaveBeenCalledWith({ anyEntityToJSONValue: "any" })
+        expect(CreateCategoryCommand).toHaveBeenCalledTimes(1)
+    })
 })
