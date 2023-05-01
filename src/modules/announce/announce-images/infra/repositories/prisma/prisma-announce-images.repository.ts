a434e4e25092cdc2a68336ae3483296821608bc9
rethred <gabrielwastha@gmail.com>
2023-04-30 22:34:20 -0300

    make prisma-announce-images-repositroy logic

--- src/modules/announce/announce-images/infra/repositories/prisma/prisma-announce-images.repository.ts
+++ src/modules/announce/announce-images/infra/repositories/prisma/prisma-announce-images.repository.ts
@@ -1,12 +1,36 @@
+import { prismaClient } from "@/modules/@shared/infra/repository/prisma/client";
 import { AnnounceImageEntity } from "../../../domain/entities";
 import { AnnounceImagesRepositoryInterface } from "../../../domain/repositories";
 
 export class PrismaAnnounceImagesRepository implements AnnounceImagesRepositoryInterface {
-    findById(id: string): Promise<AnnounceImageEntity | null> {
-        throw new Error("Method not implemented.");
+    async findById(id: string): Promise<AnnounceImageEntity | null> {
+        
+        const prismaAnnounce = await prismaClient.announce.findFirst({ where: { id } })
+        if(!prismaAnnounce) return null;
+
+        const prismaAnnounceImages = await prismaClient.announceImages.findMany({
+            where: { announceId: id }
+        })
+        const announceImageEntity = AnnounceImageEntity.create({
+            images: prismaAnnounceImages.map(img => ({ url: img.url, weight: img.weight  }))
+        }, id)
+        if(announceImageEntity.isLeft()) throw announceImageEntity.value[0]
+        return announceImageEntity.value
     }
-    update(announceImageEntity: AnnounceImageEntity): Promise<void> {
-        throw new Error("Method not implemented.");
+
+    async update(announceImageEntity: AnnounceImageEntity): Promise<void> {
+        await prismaClient.announceImages.deleteMany({
+            where: { announceId: announceImageEntity.id }
+        })
+        await prismaClient.announceImages.createMany({
+            data: [
+                ...announceImageEntity.images.map(images => ({ 
+                    weight: images.weight, 
+                    url: images.url, 
+                    announceId: announceImageEntity.id
+                }))
+            ]
+        })
     }
 
 }
