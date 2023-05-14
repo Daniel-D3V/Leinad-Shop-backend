030dd5fab44606aafa1eafc90109058e34c0dfc4
rethred <gabrielwastha@gmail.com>
2023-05-14 10:58:24 -0300

    begin allocate stock for order

---
+++ src/modules/checkout/order-inventory-management/application/usecases/allocate-stock-for-order/allocate-stock-for-order.ts
@@ -0,0 +1,11 @@
+import { UsecaseInterface } from "@/modules/@shared/domain";
+import { Either, right } from "@/modules/@shared/logic";
+
+export class AllocateStockForOrderUsecase implements UsecaseInterface {
+    async execute(input: any): Promise<Either<Error[], any>> {
+
+        
+
+        return right(null)
+    }
+}
