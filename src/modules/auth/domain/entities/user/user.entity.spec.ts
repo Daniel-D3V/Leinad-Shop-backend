2e6efc0e2b8c0d6b278c9116af305b92ed683a8e
rethred <gabrielwastha@gmail.com>
2023-05-15 17:19:59 -0300

    make userEntity unit tests

--- src/modules/auth/domain/entities/user/user.entity.spec.ts
+++ src/modules/auth/domain/entities/user/user.entity.spec.ts
@@ -1,1 +1,36 @@
-it("Should", () => {})
+import { DomainValidator } from "@/modules/@shared/domain/validator";
+import { UserEntity } from "./user.entity"
+import { UserValidatorFactory } from "./validator";
+import { mockDomainValidator } from "@/modules/@shared/tests";
+
+
+jest.mock("./validator")
+
+describe("Test UserEntity", () => {
+
+    let sut: UserEntity
+    let props: UserEntity.Input
+    let domainValidator: DomainValidator<any>
+    let id: string
+
+    beforeEach(() => {
+        id = "any_id"
+        domainValidator = mockDomainValidator()
+        jest.spyOn(UserValidatorFactory, "create").mockReturnValue(domainValidator)
+        props = {
+            username: "any_username",
+            email: "any_mail@mail.com",
+            password: "any_password"
+        }
+        sut = UserEntity.create(props, id).value as UserEntity
+    })
+
+
+    it("Should create a new UserEntity", () => {
+        sut = UserEntity.create(props, id).value as UserEntity
+        expect(sut.toJSON()).toEqual({
+            id,
+            ...props
+        })
+    })
+})
