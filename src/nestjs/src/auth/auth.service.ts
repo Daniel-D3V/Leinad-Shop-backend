842791dc9368a6e55efe4f2e8bfb60bf79b2bdb1
rethred <gabrielwastha@gmail.com>
2023-05-17 22:53:21 -0300

    config

--- src/nestjs/src/auth/auth.service.ts
+++ src/nestjs/src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AuthService {
   create(createAuthDto: any) {
+    
     console.log(createAuthDto)
     return 'This action adds a new auth';
   }
