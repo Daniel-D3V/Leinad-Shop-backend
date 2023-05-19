9871cf7f08385aeacd6312d38390b50b7553afd1
rethred <gabrielwastha@gmail.com>
2023-05-18 23:46:08 -0300

    a

--- src/core/src/index.ts
+++ src/core/src/index.ts
@@ -22,7 +22,7 @@ const program = async () => {
     statement: MySQLEvents.STATEMENTS.INSERT,
     onEvent: (event: any) => { // You will receive the events here
       console.log(event);
-      console.log(event.affectedRows)
+      console.log(event.affectedRows[0].after)
     },
   });
   
@@ -33,3 +33,36 @@ const program = async () => {
 program()
   .then(() => console.log('Waiting for database events...'))
   .catch(console.error);
+
+//   {
+//     type: 'INSERT',
+//     schema: 'test-integration-db',
+//     table: 'outbox',
+//     affectedRows: [ { after: [Object], before: undefined } ],
+//     affectedColumns: [
+//       'id',
+//       'payload',
+//       'eventName',
+//       'timestamp',
+//       'status',
+//       'error_message',
+//       'retry_count'
+//     ],
+//     timestamp: 1684464280000,
+//     nextPosition: 11118,
+//     binlogName: 'binlog.000005'
+//   }
+//   [
+//     {
+//       after: {
+//         id: '7c929350-bc03-4ce4-9d28-4cf57dc67889',
+//         payload: '{"id":"7c929350-bc03-4ce4-9d28-4cf57dc67889","eventName":"UserSignupEvent","schemaVersion":"1.0.0","dateTimeOccurred":"2023-05-19T02:44:40.200Z","payload":{"id":"f53f9f33-fea4-4d63-bcc9-f11b703f259c","username":"my username","email":"any_@gmail.com","password":"$2b$10$RKrWQxet7Ms/DKda.bNLy.zjV3QcPvZV9zl6/GOn8OEQnhbv./SOa"}}',
+//         eventName: 'UserSignupEvent',
+//         timestamp: 2023-05-19T05:44:40.200Z,
+//         status: 'PENDING',
+//         error_message: 'PENDING',
+//         retry_count: 0
+//       },
+//       before: undefined
+//     }
+//   ]
