3aa7d1538385402555ed03d6a237d6434c2f86c2
rethred <gabrielwastha@gmail.com>
2023-05-21 12:12:11 -0300

    delete-category factory

--- src/core/src/modules/category/factories/remove-category-parent.factory.ts
+++ src/core/src/modules/category/factories/remove-category-parent.factory.ts
@@ -1,21 +1,21 @@
 import { OutboxEmitter } from "@/modules/@shared/infra/providers";
-import { DeleteCategoryUsecase } from "../application/usecases";;
+import { RemoveCategoryParentUsecase } from "../application/usecases";;
 import { PrismaCategoryRepository } from "../infra/repositories";
 import { prismaClient } from "@/modules/@shared/infra/repository/prisma/client";
 import { PrismaClient } from "@prisma/client";
-import {  DeleteCategoryUsecaseInterface } from "../domain/usecases";
+import {  RemoveCategoryParentUsecaseInterface } from "../domain/usecases";
 
-export class DeleteCategoryFactory {
+export class RemoveCategoryParentFactory {
 
-    static create(): DeleteCategoryUsecaseInterface {
+    static create(): RemoveCategoryParentUsecaseInterface {
         
-        const execute = async (input: DeleteCategoryUsecaseInterface.InputDto): Promise<DeleteCategoryUsecaseInterface.OutputDto> => {
+        const execute = async (input: RemoveCategoryParentUsecaseInterface.InputDto): Promise<RemoveCategoryParentUsecaseInterface.OutputDto> => {
             
             return await prismaClient.$transaction(async (prisma) => {
                 const prismaCategoryRepository = new PrismaCategoryRepository(prisma as PrismaClient)
                 const outboxEmitter = new OutboxEmitter(prisma as PrismaClient)
-                const deleteCategoryUsecase = new DeleteCategoryUsecase(prismaCategoryRepository, outboxEmitter)
-                return await deleteCategoryUsecase.execute(input)
+                const removeCategoryParentUsecase = new RemoveCategoryParentUsecase(prismaCategoryRepository, outboxEmitter)
+                return await removeCategoryParentUsecase.execute(input)
             })
         }
 
