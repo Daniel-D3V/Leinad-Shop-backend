4bbc8adfecbe3175136316a302258bc1722d7ee2
rethred <gabrielwastha@gmail.com>
2023-05-22 08:39:19 -0300

    change modules to a folder modules

--- src/nestjs/src/app.module.ts
+++ src/nestjs/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { EventHandlerModule } from './event-handler/event-handler.module';
-import { EventSourcingModule } from './event-sourcing/event-sourcing.module';
-import { CategoriesModule } from './categories/categories.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { EventHandlerModule } from './modules/event-handler/event-handler.module';
+import { EventSourcingModule } from './modules/event-sourcing/event-sourcing.module';
+import { CategoriesModule } from './modules/categories/categories.module';
 
 @Module({
   imports: [AuthModule, EventHandlerModule, EventSourcingModule, CategoriesModule],
