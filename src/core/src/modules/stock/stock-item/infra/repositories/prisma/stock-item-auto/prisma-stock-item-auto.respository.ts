f66a13b71eaad9aec845d524faab87b95731ea6f
rethred <gabrielwastha@gmail.com>
2023-05-27 22:04:01 -0300

    prisma-stock-item-auto repository

--- src/core/src/modules/stock/stock-item/infra/repositories/prisma/stock-item-auto/prisma-stock-item-auto.respository.ts
+++ src/core/src/modules/stock/stock-item/infra/repositories/prisma/stock-item-auto/prisma-stock-item-auto.respository.ts
@@ -8,17 +8,37 @@ export class PrismaStockItemAutoRepository implements StockItemAutoRepositoryInt
         private readonly prismaClient: PrismaClient
     ){}
 
-    create(stockItemAuto: StockItemAutoEntity): Promise<void> {
-        throw new Error("Method not implemented.");
+    async create(stockItemAuto: StockItemAutoEntity): Promise<void> {
+        await this.prismaClient.stockItemAuto.create({
+            data: {
+                ...stockItemAuto.toJSON()
+            }
+        })
     }
-    delete(id: string): Promise<void> {
-        throw new Error("Method not implemented.");
+    async delete(id: string): Promise<void> {
+        await this.prismaClient.stockItemAuto.deleteMany({
+            where: { id: id ?? ""}
+        })
     }
-    update(stockItemAuto: StockItemAutoEntity): Promise<void> {
-        throw new Error("Method not implemented.");
+    async update(stockItemAuto: StockItemAutoEntity): Promise<void> {
+        const { id, stockItemId ,...props} = stockItemAuto.toJSON()
+        await this.prismaClient.stockItemAuto.update({
+            where: { id: stockItemAuto.id ?? ""},
+            data: {
+                ...props
+            }
+        })
     }
-    findById(id: string): Promise<StockItemAutoEntity | null> {
-        throw new Error("Method not implemented.");
+    async findById(id: string): Promise<StockItemAutoEntity | null> {
+        const prismaStockItemAuto = await this.prismaClient.stockItemAuto.findFirst({
+            where: { id: id ?? ""}
+        })
+        if(!prismaStockItemAuto) return null
+        const stockItemAutoEntity = StockItemAutoEntity.create({
+            ...prismaStockItemAuto
+        }, prismaStockItemAuto.id)
+        if(stockItemAutoEntity.isLeft()) throw stockItemAutoEntity.value[0]
+        return stockItemAutoEntity.value
     }
 
 }
