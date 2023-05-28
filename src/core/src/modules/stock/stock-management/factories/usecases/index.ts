b9d61da58dc4d834d35df6538ce393c2c66a0c54
rethred <gabrielwastha@gmail.com>
2023-05-27 23:24:08 -0300

    create change-stock-type-to-item-factory factory

--- src/core/src/modules/stock/stock-management/factories/usecases/index.ts
+++ src/core/src/modules/stock/stock-management/factories/usecases/index.ts
@@ -1,3 +1,4 @@
 export * from "./change-stock-type-to-auto.factory"
 export * from "./change-stock-type-to-normal.factory"
 export * from "./initialize-stock-management.factory"
+export * from "./change-stock-type-to-item.factory"
