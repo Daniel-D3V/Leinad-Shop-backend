8784d4750744cdf8931e8a11157726bd43f1260c
rethred <gabrielwastha@gmail.com>
2023-06-03 15:29:49 -0300

    finish create-announce-item usecae unit tests

--- src/core/src/modules/announce/announce-types/announce-item/application/usecases/create-announce-item/create-announce-item.spec.ts
+++ src/core/src/modules/announce/announce-types/announce-item/application/usecases/create-announce-item/create-announce-item.spec.ts
@@ -4,7 +4,9 @@ import { CreateAnnounceItemUsecaseInterface } from "../../../domain/usecases"
 import { CreateAnnounceItemUsecase } from "./create-announce-item.usecase"
 import { AnnounceItemEntity } from "../../../domain/entities"
 import { mock } from "jest-mock-extended"
+import { AnnounceItemCreatedEvent } from "./announce-item-created.event"
 
+jest.mock("./announce-item-created.event")
 
 describe("Test CreateAnnounceItemUsecase", () => {
 
@@ -34,5 +36,32 @@ describe("Test CreateAnnounceItemUsecase", () => {
     it("Should execute the usecase properly", async () => {
         const output = await sut.execute(props)
         expect(output.isRight()).toBeTruthy()
+    })
+
+    it("Should return an error if AnnounceItemEntity.create returns error", async () => {
+        const entityError = new Error("entityError")
+        jest.spyOn(AnnounceItemEntity, "create").mockReturnValue({
+            isLeft: () => true, value: entityError
+        } as any)
+        const output = await sut.execute(props)
+
+        expect(output.isLeft()).toBeTruthy()
+        expect(output.value).toEqual(entityError)
+    })
+
+    it("Should call announceItemRepository.create with correct params", async () => {
+        await sut.execute(props)
+        expect(announceItemRepository.create).toBeCalledWith(announceItemEntity)
+        expect(announceItemRepository.create).toBeCalledTimes(1)
+    })
+
+    it("Should call eventEmitter.emit once", async () => {
+        await sut.execute(props)
+        expect(eventEmitter.emit).toBeCalledTimes(1)
+    })
+
+    it("Should create an AnnounceItemCreatedEvent with correct params", async () => {
+        await sut.execute(props)
+        expect(AnnounceItemCreatedEvent).toBeCalledWith({ ...announceItemEntity.toJSON() })
     })
 })
