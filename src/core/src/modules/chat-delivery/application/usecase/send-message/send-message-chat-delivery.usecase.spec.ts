15801f214dec2562f859b443e1313f08e29dc329
Daniel-Developement <contatodanielsilvaoficial@gmail.com>
2023-06-04 11:54:32 -0300

    save

--- src/core/src/modules/chat-delivery/application/usecase/send-message/send-message-chat-delivery.usecase.spec.ts
+++ src/core/src/modules/chat-delivery/application/usecase/send-message/send-message-chat-delivery.usecase.spec.ts
@@ -3,6 +3,7 @@ import { ChatDeliveryMessageEntity } from "@/modules/chat-delivery/domain/entiti
 import { ChatDeliveryMessageRepositoryInterface } from "@/modules/chat-delivery/domain/repositories/chat-delivery-message.repository.interface"
 import { SendMessageChatDeliveryUsecaseInterface } from "@/modules/chat-delivery/domain/usecases/send-message-chat-delivery-usecase.interface"
 import { SendMessageChatDeliveryUsecase } from "./send-message-chat-delivery.usecase"
+import { mock } from "jest-mock-extended"
 
 describe("Test SendMessageChatDeliveryUsecase", () => {
 
@@ -12,7 +13,29 @@ describe("Test SendMessageChatDeliveryUsecase", () => {
     let eventEmitter: EventEmitterInterface
     let sut: SendMessageChatDeliveryUsecase
 
+    beforeEach(() => {
+        messageChatDeliveryEntity = messageChatDeliveryEntity as jest.Mocked<typeof ChatDeliveryMessageEntity>
+        messageChatDeliveryEntity.create.mockReturnValue({
+            isLeft: () => false,
+            value: {
+                toJSON: () => ({ anyEntityToJSONValue: "any" }),
+            }
+        } as any)
+
+        props = {
+            authorId: "any_id",
+            chatId: "any_chat_id",
+            content: "any_content",
+            attachments: [],
+            dateTimeSent: new Date()
+        }
+
+        messageChatDeliveryRepository = mock<ChatDeliveryMessageRepositoryInterface>()
+        eventEmitter = mock<EventEmitterInterface>()
+        sut = new SendMessageChatDeliveryUsecase(messageChatDeliveryRepository, eventEmitter)
+    })
+
     it("Should be sent messageChatDelivery", () => {
-        
+
     })
 })
