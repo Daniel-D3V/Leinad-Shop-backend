3a61ce76de32e04a303d525b2f86e262153061a8
rethred <gabrielwastha@gmail.com>
2023-06-17 11:58:43 -0300

    prisma announce info repository

---
+++ src/core/src/modules/announce/announce-info/infra/repositories/prisma/prisma-announce-info.repository.spec.ts
@@ -0,0 +1,57 @@
+import { prismaClient } from "@/modules/@shared/infra/repository/prisma/client"
+import { AnnounceInfoEntity } from "../../../domain/entities"
+import { PrismaAnnounceInfoRepository } from "./prisma-announce-info.repository"
+import { mock } from "jest-mock-extended"
+
+
+describe("Test PrismaAnnounceInfoRepository", () => {
+
+    let sut: PrismaAnnounceInfoRepository
+    let announceInfoEntity: AnnounceInfoEntity
+
+
+    beforeEach(async () => {
+
+        announceInfoEntity = mock<AnnounceInfoEntity>({
+            id: "any_id",
+            announceId: "any_announce_id",
+            toJSON: () => ({
+                id: "any_id",
+                title: "any_title",
+                description: "any_description",
+                announceId: "any_announce_id"
+            })
+        })
+
+        sut = new PrismaAnnounceInfoRepository(prismaClient)
+        await prismaClient.announceInfo.deleteMany()
+    })
+
+    it("Should create a announce info", async () => {
+        await sut.create(announceInfoEntity)
+        const createdAnnounceInfoEntity = await prismaClient.announceInfo.findUnique({
+            where: { id: announceInfoEntity.id }
+        })
+        expect(createdAnnounceInfoEntity).toEqual(announceInfoEntity.toJSON())
+    })
+
+    it("Should find a announce info by announce id", async () => {
+        await sut.create(announceInfoEntity)
+
+        const announceInfoEntityFound = await sut.findByAnnounceId(announceInfoEntity.announceId)
+
+        expect(announceInfoEntityFound?.toJSON()).toEqual(announceInfoEntity.toJSON())
+    })
+
+    it("Should return null if not found a announce info by announce id", async () => {
+        const announceInfoEntityFound = await sut.findByAnnounceId(announceInfoEntity.announceId)
+        expect(announceInfoEntityFound).toBeNull()
+    })
+
+    it("Should find a announce info  id", async () => {
+        await sut.create(announceInfoEntity)
+
+        const announceInfoEntityFound = await sut.findById(announceInfoEntity.id)
+        expect(announceInfoEntityFound?.toJSON()).toEqual(announceInfoEntity.toJSON())
+    })
+})
---
+++ src/core/src/modules/announce/announce-info/infra/repositories/prisma/prisma-announce-info.repository.ts
@@ -0,0 +1,44 @@
+import { AnnounceInfo, PrismaClient } from "@prisma/client";
+import { AnnounceInfoEntity } from "../../../domain/entities";
+import { AnnounceInfoRepositoryInterface } from "../../../domain/repositories";
+
+class PrismaAnnounceInfoMapper {
+    static toDomain(prismaAnnounceInfo: AnnounceInfo): AnnounceInfoEntity{
+        const announceInfoEntity = AnnounceInfoEntity.create({
+            ...prismaAnnounceInfo
+        }, prismaAnnounceInfo.id)
+        if(announceInfoEntity.isLeft()) throw announceInfoEntity.value[0]
+        return announceInfoEntity.value
+    }
+}
+
+export class PrismaAnnounceInfoRepository implements AnnounceInfoRepositoryInterface {
+    
+    constructor(
+        private readonly prismaClient: PrismaClient
+    ){}
+
+    async create(announceInfoEntity: AnnounceInfoEntity): Promise<void> {
+        await this.prismaClient.announceInfo.create({
+            data: {
+                ...announceInfoEntity.toJSON()
+            }
+        })
+    }
+    async findByAnnounceId(announceId: string): Promise<AnnounceInfoEntity | null> {
+        const prismaAnnounceInfo = await this.prismaClient.announceInfo.findFirst({
+            where: { announceId: announceId ?? "" }
+        })
+        if(!prismaAnnounceInfo) return null
+        return PrismaAnnounceInfoMapper.toDomain(prismaAnnounceInfo)
+    }
+    async findById(id: string): Promise<AnnounceInfoEntity | null> {
+        const prismaAnnounceInfo = await this.prismaClient.announceInfo.findFirst({
+            where: { id: id ?? "" }
+        })
+        if(!prismaAnnounceInfo) return null
+        return PrismaAnnounceInfoMapper.toDomain(prismaAnnounceInfo)
+    }
+
+    
+}
