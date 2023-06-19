a6fa519352683fc511dddcb4c7b8ef1e05f8b8a8
rethred <gabrielwastha@gmail.com>
2023-06-19 11:43:04 -0300

    finish verification code entity

--- src/core/src/modules/auth/verification-code/domain/entities/verification-code/verification-code.entity.spec.ts
+++ src/core/src/modules/auth/verification-code/domain/entities/verification-code/verification-code.entity.spec.ts
@@ -10,6 +10,7 @@ describe("Test VerificationCodeEntity", () => {
     beforeEach(() => {
         id = "any_id"
         props = {
+            userId: "any_user_id",
             code: "code",
             expirationDate: new Date()
         }
@@ -20,4 +21,38 @@ describe("Test VerificationCodeEntity", () => {
         expect(sut).toBeDefined();
         expect(sut.toJSON()).toEqual({ ...props, id });
     })
+
+    it("should create a new VerificationCodeEntity with random code", () => {
+        delete props.code
+        const sut = VerificationCodeEntity.create(props);
+        expect(sut.code).toBeDefined();
+    })
+
+    it("should create a new VerificationCodeEntity with expiration date", () => {
+        delete props.expirationDate
+        const sut = VerificationCodeEntity.create(props);
+        expect(sut.expirationDate).toBeDefined();
+    })
+
+    it("should return true if code is expired", () => {
+        const expirationDate = new Date()
+        expirationDate.setSeconds(new Date().getSeconds() - 1000)
+        const sut = VerificationCodeEntity.create({
+            ...props,
+            expirationDate
+        });
+        expect(sut.isExpired()).toBeTruthy();
+    })
+
+    it("should return false if code is not expired", () => {
+        const expirationDate = new Date()
+        expirationDate.setSeconds(new Date().getSeconds() + 1000)
+        const sut = VerificationCodeEntity.create({
+            ...props,
+            expirationDate
+        });
+        expect(sut.isExpired()).toBeFalsy();
+    })
+
+
 })
--- src/core/src/modules/auth/verification-code/domain/entities/verification-code/verification-code.entity.ts
+++ src/core/src/modules/auth/verification-code/domain/entities/verification-code/verification-code.entity.ts
@@ -7,10 +7,11 @@ export class VerificationCodeEntity extends BaseEntity<VerificationCodeEntity.Pr
         super(props, id);
     }
 
-    static create({ code, expirationDate } : VerificationCodeEntity.Input, id?: string): VerificationCodeEntity {
+    static create({ code, expirationDate, userId }: VerificationCodeEntity.Input, id?: string): VerificationCodeEntity {
         const verificationCodeEntity = new VerificationCodeEntity({
             code: code ?? VerificationCodeEntity.generateRandomCode(),
-            expirationDate: expirationDate ?? VerificationCodeEntity.generateExpirationDate()
+            expirationDate: expirationDate ?? VerificationCodeEntity.generateExpirationDate(),
+            userId
         }, id);
         return verificationCodeEntity;
     }
@@ -33,29 +34,34 @@ export class VerificationCodeEntity extends BaseEntity<VerificationCodeEntity.Pr
         return {
             id: this.id,
             code: this.code,
-            expirationDate: this.expirationDate
+            expirationDate: this.expirationDate,
+            userId: this.userId
         }
     }
 
     get code(): string {
         return this.props.code;
     }
-
     get expirationDate(): Date {
         return this.props.expirationDate;
+    }
+    get userId(): string {
+        return this.props.userId;
     }
 }
 
 export namespace VerificationCodeEntity {
 
     export type Input = {
-        code: string
-        expirationDate: Date
+        code?: string
+        expirationDate?: Date
+        userId: string
     }
 
     export type Props = {
         code: string
         expirationDate: Date
+        userId: string
     }
 
     export type PropsJSON = Props & { id: string }
