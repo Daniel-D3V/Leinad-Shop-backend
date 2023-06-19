081efe09ca496fb162c303e067fbc61e9b9ee171
rethred <gabrielwastha@gmail.com>
2023-06-19 12:06:27 -0300

    generate verification code unit tests

--- src/core/src/modules/auth/verification-code/application/usecases/generate-verification-code/generate-verification-code.spec.ts
+++ src/core/src/modules/auth/verification-code/application/usecases/generate-verification-code/generate-verification-code.spec.ts
@@ -4,7 +4,9 @@ import { GenerateVerificationCodeUsecaseInterface } from "../../../usecases/gene
 import { GenerateVerificationCodeUsecase } from "./generate-verification-code.usecase"
 import { VerificationCodeEntity } from "../../../domain/entities"
 import { mock } from "jest-mock-extended"
+import { VerificationCodeGeneratedEvent } from "./verification-code-generated.event"
 
+jest.mock("./verification-code-generated.event")
 
 describe("Test Generate Verification Code Use Case", () => {
 
@@ -20,6 +22,8 @@ describe("Test Generate Verification Code Use Case", () => {
             userId: "any_user_id"
         }
         verificationCodeEntity = mock<VerificationCodeEntity>()
+        jest.spyOn(VerificationCodeEntity, "create")
+        .mockReturnValue(verificationCodeEntity)
         verificationCodeRepository = mock<VerificationCodeRepositoryInterface>()
         eventEmitter = mock<EventEmitterInterface>()
         sut = new GenerateVerificationCodeUsecase(verificationCodeRepository, eventEmitter)
@@ -29,5 +33,30 @@ describe("Test Generate Verification Code Use Case", () => {
         const output = await sut.execute(props)
         expect(output.isRight()).toBeTruthy()
     })
-    
+
+    it("Should return VerificationCodeAlreadyGeneratedError if the verification code already exists", async () => {
+        jest.spyOn(verificationCodeRepository, "findByUserId")
+        .mockResolvedValue(true as any)
+        const output = await sut.execute(props)
+        if(output.isRight()) return fail("Should not return right")
+        expect(output.value[0].name).toBe("VerificationCodeAlreadyGeneratedError")
+    })
+
+    it("Should call verificationCodeRepository.create once", async () => {
+        await sut.execute(props)
+        expect(verificationCodeRepository.create).toHaveBeenCalledWith(verificationCodeEntity)
+        expect(verificationCodeRepository.create).toHaveBeenCalledTimes(1)
+    })
+
+    it("Should call eventEmitter.emit once", async () => {
+        await sut.execute(props)
+        expect(eventEmitter.emit).toHaveBeenCalledTimes(1)
+    })
+
+    it("Should create VerificationCodeGeneratedEvent with correct values", async () => {
+        await sut.execute(props)
+        expect(VerificationCodeGeneratedEvent).toHaveBeenCalledWith({
+            ...verificationCodeEntity.toJSON()
+        })
+    })
 })
